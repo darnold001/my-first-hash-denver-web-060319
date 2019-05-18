f4724b02c7dad9609a1529d08cd924b8f54d8e61
Devin Arnold <dbarnold321@gmail.com>
2019-05-18 03:39:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
-def my_hash (variable)
-  variable = {"key" => "vaue"}
+def my_hash
+  hash = {"key" => "value"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 
